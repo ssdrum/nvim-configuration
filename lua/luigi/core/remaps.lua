28ebb3305af12093475f241b1914b766a6aeb0ef
ssdrum <luigi.dipaolo02@gmail.com>
2023-10-21 14:03:32 +0100

    added remap to open last opened buffer

--- lua/luigi/core/remaps.lua
+++ lua/luigi/core/remaps.lua
@@ -4,3 +4,4 @@ vim.g.mapleader = " "
 local keymap = vim.keymap -- for conciseness
 
 keymap.set("n", "<leader>pv", vim.cmd.Ex) -- "n" means that this remap works while in normal mode
+keymap.set("n", "<leader> ", [[:b#<CR>]]) -- opens last opened buffer
