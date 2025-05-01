578daa92f675c5b63e4c2d82833873099fe08876
Luigi Di Paolo <luigi.dipaolo02@gmail.com>
2025-05-01 19:42:47 +0100

    Update config

--- lua/luigi/plugins/lsp/mason.lua
+++ lua/luigi/plugins/lsp/mason.lua
@@ -1,62 +1,62 @@
 -- manages installed lsps. Open with :Mason
 
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-    local mason_tool_installer = require("mason-tool-installer")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      -- list of servers to install for Mason
-      ensure_installed = {
-        "html",
-        "cssls",
-        "tsserver",
-        "lua_ls",
-        "bashls",
-        "clangd",
-        "dockerls",
-        "docker_compose_language_service",
-        "jsonls",
-        "pyright",
-        "quick_lint_js",
-        "jedi_language_server",
-        "sqlls",
-        "gopls",
-      },
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true, -- not the same as ensure_installed
-    })
+		mason_lspconfig.setup({
+			-- list of servers to install for Mason
+			ensure_installed = {
+				"html",
+				"cssls",
+				"lua_ls",
+				"bashls",
+				"clangd",
+				"dockerls",
+				"docker_compose_language_service",
+				"jsonls",
+				"pyright",
+				"quick_lint_js",
+				"jedi_language_server",
+				"sqlls",
+				"gopls",
+				"ts_ls",
+			},
+			-- auto-install configured servers (with lspconfig)
+			automatic_installation = true, -- not the same as ensure_installed
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint", -- python linter
-        "eslint_d", -- js linter
-      },
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint", -- python linter
+				"eslint_d", -- js linter
+			},
+		})
+	end,
 }
